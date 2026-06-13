f822c2707d32adde44b337fed5756267382f4a28
vedantkherade <vedantkherade76@gmail.com>
2026-06-13 20:49:59 +0530

    first commit

---
+++ mobile/constants/api.js
@@ -0,0 +1,3 @@
+// if you're using your physical phone, change this to the deployed url
+// we have explained this in the course :-)
+export const API_URL = "https://recipe-app-api-6d2w.onrender.com/api";
